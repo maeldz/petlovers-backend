46309bc4afc220950f649926be834991aec7f772
Ismael de Jesus <mael.dazareia@gmail.com>
2021-05-07 09:06:43 -0300

    feat: add auth middleware

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
+
+type Payload = {
+  id: number
+}
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' })
+  }
+  const [, token] = authHeader.split(' ')
+  try {
+    const decoded = jwt.verify(token, authConfig.secret) as Payload
+    req.userId = decoded.id
+    next()
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' })
+  }
+}
