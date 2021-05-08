c043a43086dc50a2760aade731bf68340067e8a2
Ismael de Jesus <mael.dazareia@gmail.com>
2021-05-07 22:55:03 -0300

    refactor: move auth middleware to only one assigment with router.use

--- src/routes.ts
+++ src/routes.ts
@@ -6,13 +6,11 @@ import { authMiddleware, uploadMiddleware } from '@/middlewares'
 const routes = Router()
 
 routes.post('/sessions', sessionCreateValidator, new SessionController().store)
-
 routes.post('/users', userCreateValidator, new UserController().store)
-routes.put('/users', authMiddleware, userUpdateValidator, new UserController().update)
-
-routes.post('/dogs', authMiddleware, dogCreateValidator, new DogController().store)
-routes.get('/dogs', authMiddleware, dogFindAllValidator, new DogController().index)
-
-routes.post('/files', authMiddleware, uploadMiddleware('file'), new FileController().store)
+routes.use(authMiddleware)
+routes.put('/users', userUpdateValidator, new UserController().update)
+routes.post('/dogs', dogCreateValidator, new DogController().store)
+routes.get('/dogs', dogFindAllValidator, new DogController().index)
+routes.post('/files', uploadMiddleware('file'), new FileController().store)
 
 export default routes
